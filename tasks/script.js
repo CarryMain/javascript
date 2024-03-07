53eaff10d5ac6097862450d65c9449d9dd3b6ff7
CarryMain <mr.turanov98@gmail.com>
2024-03-07 18:02:08 +0300

    fib

--- tasks/script.js
+++ tasks/script.js
@@ -98,3 +98,31 @@ function findMaxNumber(a, b, c, d) {
 }
 
 console.log(findMaxNumber(1, 5, 6.6, 11))
+
+
+// Создайте функцию, которая будет принимать в себя один аргумент-целое положительное число. Она должна возвращать строку, в которой будут через пробел выведены числа Фибоначчи. Причем, их количество должно быть равно переданному аргументу. 
+
+// Если переданный аргумент не число - вернуть пустую строку. Решать без применения рекурсии.
+
+//3 -- 0 1 1 2 3 5 8 13 21 34
+
+function fib(num) { 
+    let result = '';
+    let first = 0;
+    let second = 1;
+
+    for(let i = 0; i < num; i++) {
+        if(i + 1 === num) {
+            result += `${first}`
+        } else { 
+            result += `${first} `
+        }
+
+        let third = first + second 
+        first = second 
+        second = third 
+    }
+    return result
+}
+
+console.log(fib(21))
