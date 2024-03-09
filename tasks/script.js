b50fc59aa080036c72df846442058a8bbf32d4a6
CarryMain <mr.turanov98@gmail.com>
2024-03-09 12:34:16 +0300

    three tasks

--- tasks/script.js
+++ tasks/script.js
@@ -127,3 +127,78 @@ function fib(num) {
 
 console.log(fib(21))
 
+// 1) Напишите функцию showExperience, которая будет принимать в себя объект со всеми данными и возвращать строку с опытом.
+// Пример:
+// showExperience(personalPlanPeter) => '1 month'
+// P.S. желательно использовать деструктуризацию, но не обязательно
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    }
+};
+
+function showExperience(obj) { 
+    const {exp} = obj.skills
+    return exp
+}
+
+console.log(showExperience(personalPlanPeter))
+
+// 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
+// Пример:
+// showProgrammingLangs(personalPlanPeter)  =>
+// "Язык js изучен на 20% Язык php изучен на 10%"
+// Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
+// P.S. Для переноса строки используется \n в конце строки.
+
+function showProgrammingLangs(obj) {
+    const {programmingLangs} = obj.skills
+    let result = ''
+    for(let key in programmingLangs) {
+        result += `language ${key} learning in ${programmingLangs[key]}\n`
+    }
+    if(result === '') {
+        return ''
+    }
+    return result
+}
+
+console.log(showProgrammingLangs(personalPlanPeter))
+
+// 3) Создайте метод showAgeAndLangs внутри объекта personalPlanPeter. При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде.
+// Пример:
+// personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+// => 'Мне 29 и я владею языками: RU ENG'
+// Заметьте, что возраст и языки подставляются автоматически из объекта, а языки всегда в верхнем регистре (большими буквами). Если данные в объекте поменяются, то и сообщение тоже изменится.
+const personalPlanKirill = {
+    name: "Kirill",
+    age: "25",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '70%',
+            php: '10%',
+            ruby: '30%'
+        },
+        exp: '6 month'
+    },
+    showAgeAndLangs: function () {
+        const {languages} = this.skills
+        return `Me ${this.age} years and i have language: ${languages.join(' ').toUpperCase()}`
+    }
+};
+
+console.log(personalPlanKirill.showAgeAndLangs())
+
+
+const lang = ['Ru', 'en']
+
+console.log(lang.join(' ').toUpperCase())
