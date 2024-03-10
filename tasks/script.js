c3e10ea085f8549b8f9e67aa8c6ce017875229c0
CarryMain <mr.turanov98@gmail.com>
2024-03-10 10:43:39 +0300

    add tasks

--- tasks/script.js
+++ tasks/script.js
@@ -286,3 +286,49 @@ console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'USD'))
 const arr = [1, 2, 3]
 
 console.log(arr.includes(1))
+
+
+// Написать функцию isBudgetEnough, которая буде возвращать строку. Если бюджета хватает для отопления всего объема торгового центра - выводится 'Бюджета достаточно', если нет - 'Бюджета недостаточно'
+// Но эта задача содержит несколько подзадач внутри:
+// - вычисление общей площади всех магазинов, которая вычисляется как длина магазина, умноженная на его ширину;
+// - вычисление общего объема торгового центра, так как цена отопления указана в кубических метрах;
+// - определение того, хватает ли бюджета на оплату такого объема;
+// - все числа идут без единиц измерения для упрощения, просто цифры и все;
+// - функция должна продолжать работать, даже если изменяется количество магазинов, высота, бюджет или подставляется вообще другой объект
+
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 100000
+}
+
+function isBudgetEnough(data) {
+    let yardage = data.shops.map(item => item.area = item.width * item.length).reduce((a, b) => a + b)
+    let space = yardage * data.height
+    let volume = space * data.moneyPer1m3
+    if(volume <= data.budget) {
+        return 'Бюджета хватает'
+    }
+    return 'Бюджета не хватает'
+}
+
+console.log(isBudgetEnough(shoppingMallData))
